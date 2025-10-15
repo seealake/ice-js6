0d162383f0f8b4ce1e2eb6aadc442be108817b07
Daniel Smedema <7870631+djsmedes@users.noreply.github.com>
2025-10-15 10:30:46 -0500

    ex2

---
+++ ex2.js
@@ -0,0 +1,16 @@
+/**
+ * Detects whether a string is a palindrome.
+ * @param {string} str - The string to check.
+ * @returns {boolean}
+ */
+function isPalindrome(str) {
+  // your code here!
+  // use a C-style for loop with an early return
+}
+
+function test(str) {
+  console.log(isPalindrome(str));
+}
+
+test("racecar");
+// you may want to add more test cases
